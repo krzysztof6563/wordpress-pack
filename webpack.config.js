00ed99fc6701839292fec5e777189918bd9eea19
Krzysztof Michalski <42008046+krzysztof6563@users.noreply.github.com>
2021-04-12 11:35:57 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var Encore = require('@symfony/webpack-encore');
+var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
