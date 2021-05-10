789d7f70604d8e5ae0810778ad3646e38ee9195b
Krzysztof Michalski <42008046+krzysztof6563@users.noreply.github.com>
2021-05-10 11:37:25 +0200

    Update wp_install.sh

--- wp_install.sh
+++ wp_install.sh
@@ -85,7 +85,7 @@ function installTimber() {
 
     echo "[INFO] Adding webpack encore"
     cd wp-content/themes/timber-starter-theme
-    yarn add @symfony/webpack-encore --dev sass sass-loader bootstrap@5
+    yarn add @symfony/webpack-encore --dev sass sass-loader bootstrap@5 browser-sync-webpack-plugin browser-sync file-loader
     echo "[INFO] Copying development files"
     cp ../../../wordpress-pack/.gitignore .
     cp -r ../../../wordpress-pack/* .
