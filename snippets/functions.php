c1f1e116bf5829268ec0db50d325ba4220fb66c3
Krzysztof Michalski <42008046+krzysztof6563@users.noreply.github.com>
2023-10-25 11:16:35 +0200

    Create functions.php

---
+++ snippets/functions.php
@@ -0,0 +1,25 @@
+// To add in functions.php in your theme  
+
+/**
+ * adding CFS fields to current page/post. Add to add_to_context() fucntion
+*/
+if (get_the_ID() != 0) {
+    $context['CFS']  = CFS()->get(false);
+}
+$context['homeCFS']  = CFS()->get(false, get_option('page_on_front'));
+
+
+/**
+ * adding orphans filter to twig
+*/
+public function __construct() {
+    if (class_exists('iworks_orphan')) {
+        $this->orphan = new iworks_orphan();
+    }
+}
+public function add_to_twig($twig) {
+    $twig->addFilter(new Twig\TwigFilter('orphans', [ $this, 'remove_orphans' ]));
+}
+public function remove_orphans($text) {
+    return $this->orphan != null ? $this->orphan->replace($text) : $text;
+}
